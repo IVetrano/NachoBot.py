e49bd7154050b186b3e4b2f7580df0c5e402df8a
Ignacio Vetrano <79949866+IVetrano@users.noreply.github.com>
2022-07-02 22:23:04 -0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -26,7 +26,7 @@ async def on_member_join(member):
 async def on_message(message):
 	
 	if message.author == client.user:
-	return
+		return
 	
 	if message.content.startswith(prefix):
 	
@@ -53,43 +53,43 @@ async def on_message(message):
 			await message.channel.send(content=None, embed = embed)
 			
 		if comando == "pregunta":
-		respuestas = ["Sí", "No", "Tal vez", "No sé", "Definitivamente", " Claro "," sisi "," nono "," Por supuesto! ","No sé"," Por supuesto que no ","Desde ya que si","Desde ya que no"]
-		respuesta = respuestas[random.randint(-1, len(respuestas) - 1)]
-		embed = discord.Embed(description=respuesta)
-		await message.channel.send(content = message.author.mention + " a lo que me dijo '" + argumento + "' mi respuesta es " + respuesta, embed=embed, tts=True)
+			respuestas = ["Sí", "No", "Tal vez", "No sé", "Definitivamente", " Claro "," sisi "," nono "," Por supuesto! ","No sé"," Por supuesto que no ","Desde ya que si","Desde ya que no"]
+			respuesta = respuestas[random.randint(-1, len(respuestas) - 1)]
+			embed = discord.Embed(description=respuesta)
+			await message.channel.send(content = message.author.mention + " a lo que me dijo '" + argumento + "' mi respuesta es " + respuesta, embed=embed, tts=True)
 		
 		if comando == "reaccion":
-		respuestas = ["¿Por qué?","XD", "Vamoo", "Como en Dark Souls!", "Eso es una referencia a Homestuck", "lol","que govir", "XD", "este esta ranciovich", "Domado"]
-		respuesta = respuestas[random.randint(-1, len(respuestas) - 1)]
-		embed = discord.Embed(description=respuesta)
-		await message.channel.send(content = message.author.mention + " a lo que me dijo '" + argumento + "' mi reaccion es " + respuesta, embed=embed, tts=True)
+			respuestas = ["¿Por qué?","XD", "Vamoo", "Como en Dark Souls!", "Eso es una referencia a Homestuck", "lol","que govir", "XD", "este esta ranciovich", "Domado"]
+			respuesta = respuestas[random.randint(-1, len(respuestas) - 1)]
+			embed = discord.Embed(description=respuesta)
+			await message.channel.send(content = message.author.mention + " a lo que me dijo '" + argumento + "' mi reaccion es " + respuesta, embed=embed, tts=True)
 		
 		if comando == "equipos":
-		cantidad = len(argumento.split())
-		integrantes = argumento.split()
-		n_integrantes = cantidad/2
-		equipo1 = []
-		equipo2 = []
-		for i in range(0, cantidad):
-			if len(equipo1) < n_integrantes and len(equipo2) < n_integrantes:
-			n_equipo = random.randint(1,2)
-			if n_equipo == 1:
-				equipo1.append(integrantes[i])
-				print(integrantes[i] + " equipo1")
-			else:
+			cantidad = len(argumento.split())
+			integrantes = argumento.split()
+			n_integrantes = cantidad/2
+			equipo1 = []
+			equipo2 = []
+			for i in range(0, cantidad):
+				if len(equipo1) < n_integrantes and len(equipo2) < n_integrantes:
+				n_equipo = random.randint(1,2)
+				if n_equipo == 1:
+					equipo1.append(integrantes[i])
+					print(integrantes[i] + " equipo1")
+				else:
+					equipo2.append(integrantes[i])
+					print(integrantes[i] + " equipo2")
+				elif len(equipo1) >= n_integrantes:
 				equipo2.append(integrantes[i])
 				print(integrantes[i] + " equipo2")
-			elif len(equipo1) >= n_integrantes:
-			equipo2.append(integrantes[i])
-			print(integrantes[i] + " equipo2")
-			elif len(equipo2) >= n_integrantes:
-			equipo1.append(integrantes[i])
-			print(integrantes[i] + " equipo1")
-		embed = discord.Embed(title = "Equipos")
-		embed.add_field(name="Buenardovich", value=equipo1)
-		embed.add_field(name="VS", value="vs")
-		embed.add_field(name="Picantovic", value=equipo2)
-		await message.channel.send(content= message.author.mention + " acá tenés ranciovich", embed=embed, tts=True)
+				elif len(equipo2) >= n_integrantes:
+				equipo1.append(integrantes[i])
+				print(integrantes[i] + " equipo1")
+			embed = discord.Embed(title = "Equipos")
+			embed.add_field(name="Buenardovich", value=equipo1)
+			embed.add_field(name="VS", value="vs")
+			embed.add_field(name="Picantovic", value=equipo2)
+			await message.channel.send(content= message.author.mention + " acá tenés ranciovich", embed=embed, tts=True)
 
 		if comando == "repetirenrojo":
 			if random.choice([True, False]):
@@ -99,72 +99,72 @@ async def on_message(message):
 
 		
 		if comando == "dab":
-		await message.channel.send("https://cdn.glitch.com/6353980a-f25a-4fd8-ab49-da62c9504e1d%2F2962cbec-5cdd-47ec-98de-0bf666b698fc_DAB.png?v=1601669406341")
+			await message.channel.send("https://cdn.glitch.com/6353980a-f25a-4fd8-ab49-da62c9504e1d%2F2962cbec-5cdd-47ec-98de-0bf666b698fc_DAB.png?v=1601669406341")
 		
 		if comando == "momo":
-		sujeto = ["tu gfa ", "tu awelita ", "tu elfa ", "un grasoso ", "un holkiano ", "el peniways ", "Acosta gracioso ", "NachoGuide ",
-					"Lincrey ", "Idoboga ", "Nisman ", "Acosta pro ", "el Colo ", "Gochalo ", "el cabezon Tinelli ", "el sujeto ", "el maligno ", "el ladron ",
-					"el luz extinguido ", "la creatura ", "el Hokague ", "Dross ", "el Rubius ", "un curifeo ", "NachoBot "]
-		
-		accion = ["no te ama", "se muere", "cumple años", "mira nopor", "es de legion holk", "es grasoso", "se retira",
-					"te avienta un nokia", "esta DEVeando", "estudia", "se come un trava", "juega al LoL", "juega al DayZ", 
-					"juega al Rocket League", "no DEVea", "hace un momazo", "no va a la virgofest", "se vuelve voxerdo", "trollea",
-					"se cambia el apodo de discord", "te cambia el apodo de discord", "te banea", "es baneado", "es el impostor",
-					"es SUS", "es negro", "es racista", "hace momos en video"]
-		
-		imagenes = ["https://imgur.com/BYc6fjG", "https://imgur.com/QDXZrk3", "https://imgur.com/kjmGWW4", "https://imgur.com/dD6NEt5",
-					"https://imgur.com/hu1BbGg", "https://imgur.com/uLJcRyH", "https://imgur.com/y0kOpcu", "https://imgur.com/KxkdqkZ",
-					"https://imgur.com/RDgT2XA", "https://imgur.com/Z9bvaiW", "https://imgur.com/tVszhB6", "https://imgur.com/6JwQpVY",
-					"https://imgur.com/i9nxNxU", "https://imgur.com/KwXj5T5", "https://imgur.com/79iVWlc", "https://imgur.com/5e1WXKO",
-					"https://imgur.com/kSGqL3A", "https://imgur.com/xKQiLdx", "https://imgur.com/8tO2sj3", "https://imgur.com/fRPNkhM",
-					"https://imgur.com/OsmjvYD", "https://imgur.com/rJjfTBe", "https://imgur.com/A0oJoNq"]
-		
-		sujeto_1 = sujeto[random.randint(-1, len(sujeto) - 1)]
-		sujeto_2 = sujeto[random.randint(-1, len(sujeto) - 1)]
-		
-		accion_1 = accion[random.randint(-1, len(accion) - 1)]
-		accion_2 = accion[random.randint(-1, len(accion) - 1)]
-		
-		imagen_1 = imagenes[random.randint(-1, len(imagenes) - 1)]
-		imagen_2 = imagenes[random.randint(-1, len(imagenes) - 1)]
-		
-		res = "When " + sujeto_1 + accion_1 + "\n" + imagen_1 + "\n" + "But " + sujeto_2 + accion_2 + "\n" + imagen_2
-		await message.channel.send(content = message.author.mention + " aca tenes un momazo:")
-		await message.channel.send(content = "When " + sujeto_1 + accion_1)
-		await message.channel.send(content = imagen_1)
-		await message.channel.send(content = "But " + sujeto_2 + accion_2)
-		await message.channel.send(content = imagen_2)
-		await message.channel.send(content = ":v xdxdxdxdxdxd")
+			sujeto = ["tu gfa ", "tu awelita ", "tu elfa ", "un grasoso ", "un holkiano ", "el peniways ", "Acosta gracioso ", "NachoGuide ",
+						"Lincrey ", "Idoboga ", "Nisman ", "Acosta pro ", "el Colo ", "Gochalo ", "el cabezon Tinelli ", "el sujeto ", "el maligno ", "el ladron ",
+						"el luz extinguido ", "la creatura ", "el Hokague ", "Dross ", "el Rubius ", "un curifeo ", "NachoBot "]
+			
+			accion = ["no te ama", "se muere", "cumple años", "mira nopor", "es de legion holk", "es grasoso", "se retira",
+						"te avienta un nokia", "esta DEVeando", "estudia", "se come un trava", "juega al LoL", "juega al DayZ", 
+						"juega al Rocket League", "no DEVea", "hace un momazo", "no va a la virgofest", "se vuelve voxerdo", "trollea",
+						"se cambia el apodo de discord", "te cambia el apodo de discord", "te banea", "es baneado", "es el impostor",
+						"es SUS", "es negro", "es racista", "hace momos en video"]
+			
+			imagenes = ["https://imgur.com/BYc6fjG", "https://imgur.com/QDXZrk3", "https://imgur.com/kjmGWW4", "https://imgur.com/dD6NEt5",
+						"https://imgur.com/hu1BbGg", "https://imgur.com/uLJcRyH", "https://imgur.com/y0kOpcu", "https://imgur.com/KxkdqkZ",
+						"https://imgur.com/RDgT2XA", "https://imgur.com/Z9bvaiW", "https://imgur.com/tVszhB6", "https://imgur.com/6JwQpVY",
+						"https://imgur.com/i9nxNxU", "https://imgur.com/KwXj5T5", "https://imgur.com/79iVWlc", "https://imgur.com/5e1WXKO",
+						"https://imgur.com/kSGqL3A", "https://imgur.com/xKQiLdx", "https://imgur.com/8tO2sj3", "https://imgur.com/fRPNkhM",
+						"https://imgur.com/OsmjvYD", "https://imgur.com/rJjfTBe", "https://imgur.com/A0oJoNq"]
+			
+			sujeto_1 = sujeto[random.randint(-1, len(sujeto) - 1)]
+			sujeto_2 = sujeto[random.randint(-1, len(sujeto) - 1)]
+			
+			accion_1 = accion[random.randint(-1, len(accion) - 1)]
+			accion_2 = accion[random.randint(-1, len(accion) - 1)]
+			
+			imagen_1 = imagenes[random.randint(-1, len(imagenes) - 1)]
+			imagen_2 = imagenes[random.randint(-1, len(imagenes) - 1)]
+			
+			res = "When " + sujeto_1 + accion_1 + "\n" + imagen_1 + "\n" + "But " + sujeto_2 + accion_2 + "\n" + imagen_2
+			await message.channel.send(content = message.author.mention + " aca tenes un momazo:")
+			await message.channel.send(content = "When " + sujeto_1 + accion_1)
+			await message.channel.send(content = imagen_1)
+			await message.channel.send(content = "But " + sujeto_2 + accion_2)
+			await message.channel.send(content = imagen_2)
+			await message.channel.send(content = ":v xdxdxdxdxdxd")
 		
 		
 		
 		
 		if comando == "anime":
-		await message.channel.send("https://cdn.glitch.com/6353980a-f25a-4fd8-ab49-da62c9504e1d%2F2962cbec-5cdd-47ec-98de-0bf666b698fc_animeee3.png?v=1601669375160")
+			await message.channel.send("https://cdn.glitch.com/6353980a-f25a-4fd8-ab49-da62c9504e1d%2F2962cbec-5cdd-47ec-98de-0bf666b698fc_animeee3.png?v=1601669375160")
 		
 		if comando == "como" and argumento == " en dark souls":
-		await message.channel.send("https://youtu.be/JNLQJcv2pj4")
+			await message.channel.send("https://youtu.be/JNLQJcv2pj4")
 		
 		if comando == "bakamitai":
-		await message.channel.send("https://www.youtube.com/watch?v=VgeBUREDC3g&feature=youtu.be")
+			await message.channel.send("https://www.youtube.com/watch?v=VgeBUREDC3g&feature=youtu.be")
 		
 		if comando == "miracles":
-		await message.channel.send("https://www.youtube.com/watch?v=_-agl0pOQfs")
+			await message.channel.send("https://www.youtube.com/watch?v=_-agl0pOQfs")
 		
 		if comando == "me" and argumento == " rindo":
-		await message.channel.send("Te fuiste domado", tts=True)
+			await message.channel.send("Te fuiste domado", tts=True)
 		
 		if comando == "entragovir":
-		canal = message.author.voice.channel
-		await canal.connect()
-		await message.channel.send(message.author.mention + " ya entre al chat de voz", tts=True)
+			canal = message.author.voice.channel
+			await canal.connect()
+			await message.channel.send(message.author.mention + " ya entre al chat de voz", tts=True)
 		
 		if comando == "andate":
-		await client.user.voice_channel.disconnect()
-		await message.channel.send(message.author.mention + " Bue loco", tts=True)
+			await client.user.voice_channel.disconnect()
+			await message.channel.send(message.author.mention + " Bue loco", tts=True)
 		
 		if comando == "jefe" and argumento == " maestro":
-		await message.channel.send("Jefe Maestro, ¿Pero qué haces?, ¿que haces con la mano? ¿Que te ha dado? Que te crees ahora un gato o un animal o algo por el estilo, no me digas que te has vuelto trolo.", tts=True)
+			await message.channel.send("Jefe Maestro, ¿Pero qué haces?, ¿que haces con la mano? ¿Que te ha dado? Que te crees ahora un gato o un animal o algo por el estilo, no me digas que te has vuelto trolo.", tts=True)
 	
 		if comando == "ayuda":
 			embed = discord.Embed(title="Comandos", description="Luego de escribir 'NachoBot ' escriba:")
@@ -177,10 +177,8 @@ async def on_message(message):
 			await message.channel.send(content=None, embed=embed)
 			
 		if comando == "entra":
-		channel = message.author.voice.channel
-		await channel.connect()
-		if comando == "andate":
-		await message.voice_client.disconect()
+			channel = message.author.voice.channel
+			await channel.connect()
 
 #-------------------------------------------------------------------------------
 
