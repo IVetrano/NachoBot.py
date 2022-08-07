c326ccf6eb5d458a132b0d0abebb073880368a39
Ignacio Vetrano <79949866+IVetrano@users.noreply.github.com>
2022-08-06 22:18:58 -0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -205,6 +205,23 @@ async def on_message(message):
 				await ctx.send("Ya estoy reproduciendo govir!!")
 				return
 
+		if comando == "OdioALosChinardos":
+			YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'True'}
+			FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+			channel = message.author.voice.channel
+			voice = await channel.connect()
+			url = "https://www.youtube.com/watch?v=IoLz7OOJCFg"
+
+			if not voice.is_playing():
+				with YoutubeDL(YDL_OPTIONS) as ydl:
+					info = ydl.extract_info(url, download=False)
+				URL = info['formats'][0]['url']
+				voice.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
+				voice.is_playing()
+			else:
+				await ctx.send("Ya estoy reproduciendo govir!!")
+				return
+
 #-------------------------------------------------------------------------------
 
 #def hola():
